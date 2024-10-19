f57fd0cfe4e257012c21d26cc8b3a3805ebb9def
IhlasulMufti <ihlasul05mufti@gmail.com>
2024-10-20 00:14:34 +0800

    Day 17 - Page Object Page Factor (Recruitment Page Test: OrangeHRM Case)

---
+++ src/test/java/com/juaracoding/RecruitmentTest.java
@@ -0,0 +1,47 @@
+package com.juaracoding;
+
+import com.juaracoding.drivers.DriverSingleton;
+import com.juaracoding.pages.LoginPage;
+import com.juaracoding.pages.RecruitmentPage;
+import com.juaracoding.utils.Utils;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class RecruitmentTest {
+
+    private WebDriver driver;
+
+    private LoginPage loginPage;
+    private RecruitmentPage recruitmentPage;
+
+    @BeforeClass
+    public void setUp() {
+        DriverSingleton.getInstance("firefox");
+        driver = DriverSingleton.getDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        loginPage = new LoginPage();
+        recruitmentPage = new RecruitmentPage();
+    }
+
+    @AfterClass
+    public void finish() {
+        Utils.delay(5);
+        DriverSingleton.closeObjectInstance();
+    }
+
+    @Test
+    public void testAddCandidate() {
+        // step pre-condition:
+        loginPage.loginUser("Admin", "admin123");
+
+        // step action:
+        recruitmentPage.setMenuRecruitment();
+        recruitmentPage.addCandidate();
+        // step validation:
+        Assert.assertEquals(recruitmentPage.getTxtApplicationStage(), "Application Stage");
+        Assert.assertEquals(recruitmentPage.getTxtFirstValidation(), "Juara Coding");
+    }
+}
